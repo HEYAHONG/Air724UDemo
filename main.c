b9d7a974ae5d75fecd951762fbe8226429f934ef
HEYAHONG <2229388563@qq.com>
2022-04-07 17:22:48 +0800

    添加对IMEI的打印

--- main.c
+++ main.c
@@ -9,6 +9,9 @@
 #include "appstack.hpp"
 #include "network.h"
 #include "iot_pmd.h"
+#include "at_process.h"
+#include "at_tok.h"
+
 
 HANDLE main_task_handle=NULL;
 
@@ -16,11 +19,60 @@ uint64_t ms_per_tick=5;
 
 static  __unused  const char * TAG="main";
 
+static char imei[32]= {0};//存储IMEI
+static bool gsmGetIMEI(char* imeiOut)
+{
+    int err;
+    ATResponse *p_response = NULL;
+    char* line = NULL;
+    //UINT8 index = 0;
+    bool result = FALSE;
+    if(!imeiOut)
+    {
+        return result;
+    }
+
+    err = at_send_command_numeric("AT+GSN", &p_response);
+    if (err < 0 || p_response->success==0)
+    {
+        result = FALSE;
+        goto end;
+    }
+
+    line = p_response->p_intermediates->line;
+
+    if(line!=NULL)
+    {
+        strcpy(imeiOut,line);
+    }
+    result = TRUE;
+end:
+    at_response_free(p_response);
+    return result;
+
+}
+const char * get_imei()
+{
+    if(strlen(imei)!=0)
+    {
+        return imei;
+    }
+    if(gsmGetIMEI(imei))
+    {
+        return imei;
+    }
+
+    return NULL;
+}
+
+
 static void main_task(PVOID pParameter)
 {
 
 
     app_debug_print("%s:%s",TAG,CONFIG_APP_ENTER_MESSAGE"\n\r");
+
+
 
     {
         uint64_t current_tick=iot_os_get_system_tick();
@@ -30,6 +82,16 @@ static void main_task(PVOID pParameter)
         app_debug_print("%s:current tick=%u after 500ms\n\r",TAG,current_tick_after_500ms);
         ms_per_tick=(500/(current_tick_after_500ms-current_tick));
         app_debug_print("%s:ms_per_tick=%u\n\r",TAG,ms_per_tick);
+    }
+
+    {
+        //获取IMEI
+        while(get_imei()==NULL)
+        {
+            iot_os_sleep(2000);//延时2000ms
+            app_debug_print("%s:wait for imei\n\r",TAG);
+        }
+        app_debug_print("%s:imei:%s\n\r",TAG,get_imei());
     }
 
 
